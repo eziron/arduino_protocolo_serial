feeb2ab49f093111086a659b3124e6990c226aeb
Eziron <kraedon.rf@gmail.com>
2022-04-02 13:40:07 -0300

    A

--- protocolo_serial.cpp
+++ protocolo_serial.cpp
@@ -4,13 +4,12 @@
 
 ProtocoloSerial::ProtocoloSerial(){}
 
-bool ProtocoloSerial::read_command(uint8_t *out_rx_buffer, uint32_t time_out){
+bool ProtocoloSerial::read_command(uint8_t *out_rx_buffer,uint8_t *cmd, uint8_t *d_len, uint8_t *d_type, uint32_t time_out){
     if(time_out > 0){
-        wait_lidar_response(time_out);
+        wait_response(time_out);
     }
     while (port.available() > 0) {
         rx_c = port.read();
-
         if(
             (rx_count == 0 && rx_c == synq_byte1) ||
             (rx_count == 1 && rx_c == synq_byte2) ||
@@ -20,8 +19,31 @@ bool ProtocoloSerial::read_command(uint8_t *out_rx_buffer, uint32_t time_out){
             rx_count++;
 
             if(rx_lim_en){
+                if (rx_count == rx_lim) {
+                    for(int i = 5;i<rx_lim;i++){
+                        out_rx_buffer[i-5] = prev_rx_buffer[i];
+                    }
+                    *cmd    = prev_rx_buffer[2];
+                    *d_type = prev_rx_buffer[3];
+                    *d_len  = prev_rx_buffer[4];
+                    
+                    rx_count = 0;
+                    return true;
+                }
+            }
+            else{
                 if(rx_count == 5){
                     switch (prev_rx_buffer[3]){
+                        case 'b':
+                            rx_lim = rx_c+5;
+                            rx_lim_en = true;
+                            break;
+                        
+                        case 'B':
+                            rx_lim = rx_c+5;
+                            rx_lim_en = true;
+                            break;
+
                         case 'h':
                             rx_lim = (rx_c*2)+5;
                             rx_lim_en = true;
@@ -70,100 +92,110 @@ bool ProtocoloSerial::read_command(uint8_t *out_rx_buffer, uint32_t time_out){
                     }
                 }
             }
-            else{
-                if (rx_count == rx_lim) {
-                    for(int i = 0;i<rx_lim;i++){
-                        out_rx_buffer[i] = prev_rx_buffer[i];
-                    }
-                    rx_count = 0;
-                    return true;
-                }
-            }
         }
         else {
             rx_count = 0;
+            rx_lim_en = false;
             return false;
         }
     }
     return false;
 }
 
-uint16_t *decode_uint16(uint8_t byte_buffer[]){
-    uint8_t n_bytes = byte_buffer[4];
+void ProtocoloSerial::decode_uint16(uint8_t byte_buffer[], uint8_t d_len, uint16_t *out_buffer){
     uint8_t n_base;
-    uint16_t r[n_bytes];
-    for(i=0;i<n_bytes;i++){
-        n_base = (i*2)+5;
-        r[i] = byte_buffer[n_base]<<8 || byte_buffer[n_base+1];
+    for(uint8_t i=0;i<d_len;i++){
+        n_base = (i*2);
+        out_buffer[i] = (uint16_t) (byte_buffer[n_base]<<8) | byte_buffer[n_base+1];
     }
-    return r;
 }
 
-int16_t  *decode_int16(uint8_t byte_buffer[]){
-    uint8_t n_bytes = byte_buffer[4];
+void ProtocoloSerial::decode_int16(uint8_t byte_buffer[], uint8_t d_len, int16_t *out_buffer){
     uint8_t n_base;
-    int16_t r[n_bytes];
-    for(i=0;i<n_bytes;i++){
-        n_base = (i*2)+5;
-        r[i] = byte_buffer[n_base]<<8 || byte_buffer[n_base+1];
+    for(uint8_t i=0;i<d_len;i++){
+        n_base = (i*2);
+        out_buffer[i] = (int16_t) (byte_buffer[n_base]<<8) | byte_buffer[n_base+1];
     }
-    return r;
 }
 
-uint32_t *decode_uint32(uint8_t byte_buffer[]){
-    uint8_t n_bytes = byte_buffer[4];
+void ProtocoloSerial::decode_uint32(uint8_t byte_buffer[], uint8_t d_len, uint32_t *out_buffer){
     uint8_t n_base;
-    uint32_t r[n_bytes];
-    for(i=0;i<n_bytes;i++){
-        n_base = (i*2)+5;
-        r[i] = byte_buffer[n_base]<<24 || byte_buffer[n_base+1]<<16 || byte_buffer[n_base+2]<<8 || byte_buffer[n_base+3];
+    for(uint8_t i=0;i<d_len;i++){
+        n_base = (i*4);
+        out_buffer[i] = (uint32_t) (byte_buffer[n_base]<<24) | (byte_buffer[n_base+1]<<16) | (byte_buffer[n_base+2]<<8) | byte_buffer[n_base+3];
     }
-    return r;
 }
 
-int32_t  *decode_int32(uint8_t byte_buffer[]){
-    uint8_t n_bytes = byte_buffer[4];
+void ProtocoloSerial::decode_int32(uint8_t byte_buffer[], uint8_t d_len, int32_t *out_buffer){
     uint8_t n_base;
-    int32_t r[n_bytes];
-    for(i=0;i<n_bytes;i++){
-        n_base = (i*2)+5;
-        r[i] = byte_buffer[n_base]<<24 || byte_buffer[n_base+1]<<16 || byte_buffer[n_base+2]<<8 || byte_buffer[n_base+3];
+    for(uint8_t i=0;i<d_len;i++){
+        n_base = (i*4);
+        out_buffer[i] = (int32_t) (byte_buffer[n_base]<<24) | (byte_buffer[n_base+1]<<16) | (byte_buffer[n_base+2]<<8) | byte_buffer[n_base+3];
     }
-    return r;
 }
 
-uint64_t *decode_uint64(uint8_t byte_buffer[]){
-    uint8_t n_bytes = byte_buffer[4];
+void ProtocoloSerial::decode_uint64(uint8_t byte_buffer[], uint8_t d_len, uint64_t *out_buffer){
     uint8_t n_base;
-    uint64_t r[n_bytes];
-    for(i=0;i<n_bytes;i++){
-        n_base = (i*2)+5;
-        r[i] =  byte_buffer[n_base]<<56 || byte_buffer[n_base+1]<<48 || byte_buffer[n_base+2]<<40 || byte_buffer[n_base+3]<<32 || 
-                byte_buffer[n_base+4]<<24 || byte_buffer[n_base+5]<<16 || byte_buffer[n_base+6]<<8 || byte_buffer[n_base+7];
+    for(uint8_t i=0;i<d_len;i++){
+        n_base = (i*8);
+        out_buffer[i] = 0;
+        for(uint8_t j = 7;j>=0;j--){
+            out_buffer[i] = out_buffer[i]<<8;
+            out_buffer[i] |= (uint64_t)byte_buffer[n_base+j];
+        }
+        //out_buffer[i] = (uint64_t) (byte_buffer[n_base]<<56) || (byte_buffer[n_base+1]<<48) || (byte_buffer[n_base+2]<<40) || (byte_buffer[n_base+3]<<32) || (byte_buffer[n_base+4]<<24) || (byte_buffer[n_base+5]<<16) || (byte_buffer[n_base+6]<<8) || byte_buffer[n_base+7];
     }
-    return r;
 }
 
-int64_t  *decode_int64(uint8_t byte_buffer[]){
-    uint8_t n_bytes = byte_buffer[4];
+void ProtocoloSerial::decode_int64(uint8_t byte_buffer[], uint8_t d_len, int64_t *out_buffer){
     uint8_t n_base;
-    int64_t r[n_bytes];
-    for(i=0;i<n_bytes;i++){
-        n_base = (i*2)+5;
-        r[i] =  byte_buffer[n_base]<<56 || byte_buffer[n_base+1]<<48 || byte_buffer[n_base+2]<<40 || byte_buffer[n_base+3]<<32 || 
-                byte_buffer[n_base+4]<<24 || byte_buffer[n_base+5]<<16 || byte_buffer[n_base+6]<<8 || byte_buffer[n_base+7];
+    for(uint8_t i=0;i<d_len;i++){
+        n_base = (i*8);
+        out_buffer[i] = 0;
+        for(uint8_t j = 7;j>=0;j--){
+            out_buffer[i] = out_buffer[i]<<8;
+            out_buffer[i] |= (uint64_t)byte_buffer[n_base+j];
+        }
+        //out_buffer[i] = (int64_t) (byte_buffer[n_base]<<56) || (byte_buffer[n_base+1]<<48) || (byte_buffer[n_base+2]<<40) || (byte_buffer[n_base+3]<<32) || (byte_buffer[n_base+4]<<24) || (byte_buffer[n_base+5]<<16) || (byte_buffer[n_base+6]<<8) || byte_buffer[n_base+7];
     }
-    return r;
 }
 
-
-
-void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, int16_t *data_array){
+void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, int8_t data_array[]){
     tx_buffer[0] = synq_byte1;
     tx_buffer[1] = synq_byte2;
 
     tx_buffer[2] = cmd;
-    tx_buffer[3] = "h";
+    tx_buffer[3] = 'b';
+    tx_buffer[4] = d_len;
+
+    for(uint8_t i = 0; i<d_len;i++){
+        tx_buffer[i+5] = data_array[i];
+    }
+
+    port.write(tx_buffer,d_len+5);
+}
+
+void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, uint8_t data_array[]){
+    tx_buffer[0] = synq_byte1;
+    tx_buffer[1] = synq_byte2;
+
+    tx_buffer[2] = cmd;
+    tx_buffer[3] = 'B';
+    tx_buffer[4] = d_len;
+
+    for(uint8_t i = 0; i<d_len;i++){
+        tx_buffer[i+5] = data_array[i];
+    }
+
+    port.write(tx_buffer,d_len+5);
+}
+
+void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, int16_t data_array[]){
+    tx_buffer[0] = synq_byte1;
+    tx_buffer[1] = synq_byte2;
+
+    tx_buffer[2] = cmd;
+    tx_buffer[3] = 'h';
     tx_buffer[4] = d_len;
 
     for(uint8_t i = 0; i<d_len;i++){
@@ -176,12 +208,12 @@ void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, int16_t *data_arr
     port.write(tx_buffer,(d_len*2)+5);
 }
 
-void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, short *data_array){
+void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, uint16_t data_array[]){
     tx_buffer[0] = synq_byte1;
     tx_buffer[1] = synq_byte2;
 
     tx_buffer[2] = cmd;
-    tx_buffer[3] = "h";
+    tx_buffer[3] = 'H';
     tx_buffer[4] = d_len;
 
     for(uint8_t i = 0; i<d_len;i++){
@@ -194,48 +226,12 @@ void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, short *data_array
     port.write(tx_buffer,(d_len*2)+5);
 }
 
-void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, uint16_t *data_array){
+void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, int32_t data_array[]){
     tx_buffer[0] = synq_byte1;
     tx_buffer[1] = synq_byte2;
 
     tx_buffer[2] = cmd;
-    tx_buffer[3] = "H";
-    tx_buffer[4] = d_len;
-
-    for(uint8_t i = 0; i<d_len;i++){
-        uint8_t n_base = (i*2)+5;
-
-        tx_buffer[n_base] = data_array[i]>>8;
-        tx_buffer[n_base+1] = data_array[i];
-    }
-
-    port.write(tx_buffer,(d_len*2)+5);
-}
-
-void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, unsigned short *data_array){
-    tx_buffer[0] = synq_byte1;
-    tx_buffer[1] = synq_byte2;
-
-    tx_buffer[2] = cmd;
-    tx_buffer[3] = "H";
-    tx_buffer[4] = d_len;
-
-    for(uint8_t i = 0; i<d_len;i++){
-        uint8_t n_base = (i*2)+5;
-
-        tx_buffer[n_base] = data_array[i]>>8;
-        tx_buffer[n_base+1] = data_array[i];
-    }
-
-    port.write(tx_buffer,(d_len*2)+5);
-}
-
-void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, int32_t *data_array){
-    tx_buffer[0] = synq_byte1;
-    tx_buffer[1] = synq_byte2;
-
-    tx_buffer[2] = cmd;
-    tx_buffer[3] = "i";
+    tx_buffer[3] = 'i';
     tx_buffer[4] = d_len;
 
     for(uint8_t i = 0; i<d_len;i++){
@@ -250,12 +246,12 @@ void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, int32_t *data_arr
     port.write(tx_buffer,(d_len*4)+5);
 }
 
-void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, int *data_array){
+void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, uint32_t data_array[]){
     tx_buffer[0] = synq_byte1;
     tx_buffer[1] = synq_byte2;
 
     tx_buffer[2] = cmd;
-    tx_buffer[3] = "i";
+    tx_buffer[3] = 'I';
     tx_buffer[4] = d_len;
 
     for(uint8_t i = 0; i<d_len;i++){
@@ -270,92 +266,12 @@ void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, int *data_array){
     port.write(tx_buffer,(d_len*4)+5);
 }
 
-void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, long *data_array){
+void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, int64_t data_array[]){
     tx_buffer[0] = synq_byte1;
     tx_buffer[1] = synq_byte2;
 
     tx_buffer[2] = cmd;
-    tx_buffer[3] = "l";
-    tx_buffer[4] = d_len;
-
-    for(uint8_t i = 0; i<d_len;i++){
-        uint8_t n_base = (i*4)+5;
-
-        tx_buffer[n_base] = data_array[i]>>24;
-        tx_buffer[n_base+1] = data_array[i]>>16;
-        tx_buffer[n_base+2] = data_array[i]>>8;
-        tx_buffer[n_base+3] = data_array[i];
-    }
-
-    port.write(tx_buffer,(d_len*4)+5);
-}
-
-void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, uint32_t *data_array){
-    tx_buffer[0] = synq_byte1;
-    tx_buffer[1] = synq_byte2;
-
-    tx_buffer[2] = cmd;
-    tx_buffer[3] = "I";
-    tx_buffer[4] = d_len;
-
-    for(uint8_t i = 0; i<d_len;i++){
-        uint8_t n_base = (i*4)+5;
-
-        tx_buffer[n_base] = data_array[i]>>24;
-        tx_buffer[n_base+1] = data_array[i]>>16;
-        tx_buffer[n_base+2] = data_array[i]>>8;
-        tx_buffer[n_base+3] = data_array[i];
-    }
-
-    port.write(tx_buffer,(d_len*4)+5);
-}
-
-void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, unsigned int *data_array){
-    tx_buffer[0] = synq_byte1;
-    tx_buffer[1] = synq_byte2;
-
-    tx_buffer[2] = cmd;
-    tx_buffer[3] = "I";
-    tx_buffer[4] = d_len;
-
-    for(uint8_t i = 0; i<d_len;i++){
-        uint8_t n_base = (i*4)+5;
-
-        tx_buffer[n_base] = data_array[i]>>24;
-        tx_buffer[n_base+1] = data_array[i]>>16;
-        tx_buffer[n_base+2] = data_array[i]>>8;
-        tx_buffer[n_base+3] = data_array[i];
-    }
-
-    port.write(tx_buffer,(d_len*4)+5);
-}
-
-void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, unsigned long *data_array){
-    tx_buffer[0] = synq_byte1;
-    tx_buffer[1] = synq_byte2;
-
-    tx_buffer[2] = cmd;
-    tx_buffer[3] = "L";
-    tx_buffer[4] = d_len;
-
-    for(uint8_t i = 0; i<d_len;i++){
-        uint8_t n_base = (i*4)+5;
-
-        tx_buffer[n_base] = data_array[i]>>24;
-        tx_buffer[n_base+1] = data_array[i]>>16;
-        tx_buffer[n_base+2] = data_array[i]>>8;
-        tx_buffer[n_base+3] = data_array[i];
-    }
-
-    port.write(tx_buffer,(d_len*4)+5);
-}
-
-void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, int64_t *data_array){
-    tx_buffer[0] = synq_byte1;
-    tx_buffer[1] = synq_byte2;
-
-    tx_buffer[2] = cmd;
-    tx_buffer[3] = "q";
+    tx_buffer[3] = 'q';
     tx_buffer[4] = d_len;
 
     for(uint8_t i = 0; i<d_len;i++){
@@ -374,60 +290,12 @@ void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, int64_t *data_arr
     port.write(tx_buffer,(d_len*8)+5);
 }
 
-void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, long long *data_array){
+void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, uint64_t data_array[]){
     tx_buffer[0] = synq_byte1;
     tx_buffer[1] = synq_byte2;
 
     tx_buffer[2] = cmd;
-    tx_buffer[3] = "q";
-    tx_buffer[4] = d_len;
-
-    for(uint8_t i = 0; i<d_len;i++){
-        uint8_t n_base = (i*8)+5;
-
-        tx_buffer[n_base] = data_array[i]>>56;
-        tx_buffer[n_base+1] = data_array[i]>>48;
-        tx_buffer[n_base+2] = data_array[i]>>40;
-        tx_buffer[n_base+3] = data_array[i]>>32;
-        tx_buffer[n_base+4] = data_array[i]>>24;
-        tx_buffer[n_base+5] = data_array[i]>>16;
-        tx_buffer[n_base+6] = data_array[i]>>8;
-        tx_buffer[n_base+7] = data_array[i];
-    }
-
-    port.write(tx_buffer,(d_len*8)+5);
-}
-
-void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, uint64_t *data_array){
-    tx_buffer[0] = synq_byte1;
-    tx_buffer[1] = synq_byte2;
-
-    tx_buffer[2] = cmd;
-    tx_buffer[3] = "Q";
-    tx_buffer[4] = d_len;
-
-    for(uint8_t i = 0; i<d_len;i++){
-        uint8_t n_base = (i*8)+5;
-
-        tx_buffer[n_base] = data_array[i]>>56;
-        tx_buffer[n_base+1] = data_array[i]>>48;
-        tx_buffer[n_base+2] = data_array[i]>>40;
-        tx_buffer[n_base+3] = data_array[i]>>32;
-        tx_buffer[n_base+4] = data_array[i]>>24;
-        tx_buffer[n_base+5] = data_array[i]>>16;
-        tx_buffer[n_base+6] = data_array[i]>>8;
-        tx_buffer[n_base+7] = data_array[i];
-    }
-
-    port.write(tx_buffer,(d_len*8)+5);
-}
-
-void ProtocoloSerial::send_command(uint8_t cmd, uint8_t d_len, unsigned long long *data_array){
-    tx_buffer[0] = synq_byte1;
-    tx_buffer[1] = synq_byte2;
-
-    tx_buffer[2] = cmd;
-    tx_buffer[3] = "Q";
+    tx_buffer[3] = 'Q';
     tx_buffer[4] = d_len;
 
     for(uint8_t i = 0; i<d_len;i++){
